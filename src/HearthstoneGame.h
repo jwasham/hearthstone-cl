cfd5c0af71d59c586b78d0206de2da15e1ec2cd1
John Washam <john@readyprompt.com>
2016-06-02 14:58:27 -0700

    Completing test harness install.

--- src/HearthstoneGame.h
+++ src/HearthstoneGame.h
@@ -6,29 +6,30 @@
 
 namespace HearthstoneCL {
 
-class HearthstoneGame {
- private:
-  bool debugMode{false};
-  bool strictDecks{true};
-  Player player1;
-  Player player2;
-  const std::string deckDirectory{"decks"};
-  void setupPlayer(const std::string deckFileName, Player &player);
-  // void deckCheck();
+  class HearthstoneGame {
+   private:
+    bool debugMode{false};
+    bool strictDecks{true};
+    Player player1;
+    Player player2;
+    const std::string deckDirectory{"decks"};
+    void setupPlayer(const std::string deckFileName, Player &player);
+    // void deckCheck();
 
- public:
-  static void showUsage();
+   public:
+    static void showUsage();
 
-  HearthstoneGame();
-  HearthstoneGame(const HearthstoneGame &h){};  // dummy copy constructor
-  HearthstoneGame &operator=(const HearthstoneGame &h) {
-    return *this;
-  };  // dummy assignment operator
-  virtual ~HearthstoneGame();
-  void start(const std::string &deck1, const std::string &deck2);
-  void enableDebugMode();
-  void disableStrictDecks();
-};
-};
+    HearthstoneGame();
+    HearthstoneGame(const HearthstoneGame &h){};  // dummy copy constructor
+    HearthstoneGame &operator=(const HearthstoneGame &h) {
+      return *this;
+    };  // dummy assignment operator
+    virtual ~HearthstoneGame();
+    void start(const std::string &deck1, const std::string &deck2);
+    void enableDebugMode();
+    void disableStrictDecks();
+  };
+
+}
 
 #endif  // HEARTHSTONE_CL_HEARTHSTONEGAME_H
---
+++ tests/tests/argument_check.cpp
@@ -0,0 +1,5 @@
+#include "gtest/gtest.h"
+
+TEST(argument_check, test_eq) {
+  EXPECT_EQ(1, 0);
+}
